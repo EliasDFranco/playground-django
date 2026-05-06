03b6a99506bcd46ead13ba5c3bb2da993baf9f07
Elias Franco Duarte <eliasdfranco@hotmail.com>
2026-05-06 17:57:33 -0300

    Regresando html

--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
@@ -13,16 +13,26 @@ days_of_week = {
     "sunday": "Hoy se come asado",
 }
 
+def index(request):
+    listItems = ""
+    days = list(days_of_week.keys()) # monday, thursday
+    for day in days:
+        day_path = reverse("day-quote", args=[day])
+        listItems += f"<li><a href=\'{day_path}\'>{day}</a></li>"
+    responseData = f"<ul>{listItems}</ul>"
+    return HttpResponse(responseData)
+    
 def days_week_with_number(request, day):
     days = list(days_of_week.keys())
     if day > len(days):
         return HttpResponseNotFound("Este día no existe")
     redirect_day = days[day - 1]
-    redirect_path = reversed("day-quote", args=[redirect_day])
+    redirect_path = reverse("day-quote", args=[redirect_day])
     return HttpResponseRedirect(redirect_path)
 
 def days_week(request, day):
     try:
+        quoteText = days_of_week[day]
         return HttpResponse(quoteText)
     except KeyError:
         return HttpResponseNotFound("Error: Esto no es un día de la semana")
