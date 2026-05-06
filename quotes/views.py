8b4ecc817ea420866f4689f5fecb5d1da5067857
Elias Franco Duarte <eliasdfranco@hotmail.com>
2026-05-06 16:58:53 -0300

    Función reversiva

--- quotes/views.py
+++ quotes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 days_of_week = {
-  "monday": "Pienso luego existo",
-  "tuesday": "Me chingué la rodilla",
-  "wednesday": "Ay caramba",
-  "thursday": "Hoy toca porque toca",
-  "friday": "Vamonos al after office",
-  "saturday": "En el séptimo día descanso",
-  "sunday": "Hoy se come asado",
+    "monday": "Pienso luego existo",
+    "tuesday": "Me chingué la rodilla",
+    "wednesday": "Ay caramba",
+    "thursday": "Hoy toca porque toca",
+    "friday": "Vamonos al after office",
+    "saturday": "En el séptimo día descanso",
+    "sunday": "Hoy se come asado",
 }
 
 def days_week_with_number(request, day):
@@ -17,12 +18,11 @@ def days_week_with_number(request, day):
     if day > len(days):
         return HttpResponseNotFound("Este día no existe")
     redirect_day = days[day - 1]
-    return HttpResponseRedirect(f"/quotes/{redirect_day}")
+    redirect_path = reversed("day-quote", args=[redirect_day])
+    return HttpResponseRedirect(redirect_path)
 
 def days_week(request, day):
     try:
-        quoteText = days_of_week[days]
         return HttpResponse(quoteText)
-    except:
+    except KeyError:
         return HttpResponseNotFound("Error: Esto no es un día de la semana")
-  
