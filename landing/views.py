4910cab8857e020784dffcf0a80ec04472c4f5d7
Elias Franco Duarte <eliasdfranco@hotmail.com>
2026-05-11 18:07:09 -0300

    Interpolación de variables

--- landing/views.py
+++ landing/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Home(request):
-  return render(request,"landing/template.html")
+  return render(request,"landing/template.html", {
+    "nombre": "Elias",
+    "apellido": "Franco"
+  })
